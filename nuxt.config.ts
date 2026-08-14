6755fef294d1ddca1800bcd3a49dea2e4f777934
JB Hutch <jbhutch01@gmail.com>
2026-08-14 09:05:29 -0400

    revert: remove Nuxt preview fixture

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2026-08-14',
-  modules: ['@nuxt/content'],
-  content: {
-    experimental: {
-      sqliteConnector: 'native'
-    }
-  }
-})
